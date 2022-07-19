fe0faeaf3be75a254892cb7cefe07267666ec195
Rogue-striker <jyothikiran52@gmail.com>
2022-07-19 18:34:02 +0530

    added heroku link

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,8 @@ import { nanoid } from 'nanoid';
 import React, { useState, useEffect } from 'react'
 import { io } from 'socket.io-client';
 
-const socket = io("http://localhost:5000");
+const socket = io("https://prechat-backend.herokuapp.com/");
+
 const HomePage = () => {
   const [registered, setRegistered] = useState(false);
   const [userMessage, setUserMessage] = useState("");
